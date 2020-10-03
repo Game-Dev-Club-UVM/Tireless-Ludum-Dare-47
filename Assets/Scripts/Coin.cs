3f7fbe7366539758ee0e29bf274a46b46f303dc9
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-10-03 15:48:26 -0400

    maded coin bob and spin prefab

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+	public Transform coinGFX;
+
+    [SerializeField]
+    float bobDistance = .5f;
+    [SerializeField]
+    float rotateSpeed = 2f;
+    [SerializeField]
+    float bobSpeed = 1f;
+
+	bool isGoingUp;
+
+	float startPos;
+	private void Awake()
+	{
+		startPos = coinGFX.position.y;
+		isGoingUp = Random.Range(0, 2) == 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		RotateCoin();
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Coin Trigger");
+	}
+
+    void RotateCoin()
+	{
+		coinGFX.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+		if (isGoingUp)
+		{
+			coinGFX.Translate(new Vector3(0, bobSpeed * Time.deltaTime, 0));
+		}
+		else
+		{
+			coinGFX.Translate(new Vector3(0, -bobSpeed * Time.deltaTime, 0));
+		}
+
+        if(coinGFX.position.y >= startPos + bobDistance)
+		{
+			isGoingUp = false;
+		}
+		else if(coinGFX.position.y <= startPos - bobDistance)
+		{
+			isGoingUp = true;
+		}
+	}
+}
