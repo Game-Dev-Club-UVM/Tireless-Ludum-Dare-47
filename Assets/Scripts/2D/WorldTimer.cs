fae829189b8c80c0c5818b16f290affbab6f16ca
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-10-03 10:39:13 -0400

    top down movement

---
+++ Assets/Scripts/2D/WorldTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldTimer : MonoBehaviour
+{
+    public float ResetTime = 30;
+    [SerializeField]
+    private float TimeElipsed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeElipsed += Time.deltaTime;
+
+        if(TimeElipsed>= ResetTime)
+		{
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+    }
+}
