de8f0f971f9d371c60244098a86518a05bc092a3
Sam Gusick <sgusick@uvm.edu>
2020-10-04 17:37:15 -0400

    TIRE MARXXX :+1:

---
+++ Assets/trailScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailScript : MonoBehaviour
+{
+
+
+    public Transform playerTransform;
+    private void Awake() {
+        playerTransform = GameObject.Find("TireModel").GetComponent<Transform>();
+    }
+    private void FixedUpdate() {
+        transform.position = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
+    }
+}
